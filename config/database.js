abfae7d69dea9cafbbc3b22ad8d057f3ff17edbd
bear <bear@kali.bear>
2026-01-13 22:34:43 +0600

    Fix database configuration to ensure case-insensitive DATABASE_TYPE check

--- config/database.js
+++ config/database.js
@@ -1,6 +1,8 @@
 // Switch between SQLite and MySQL based on DATABASE_TYPE environment variable
 // Set DATABASE_TYPE=sqlite for SQLite, DATABASE_TYPE=mysql for MySQL, default to SQLite
-const { db, initDatabase } = (process.env.DATABASE_TYPE === 'mysql')
+// Explicitly check for 'mysql' value, otherwise default to SQLite
+const databaseType = process.env.DATABASE_TYPE ? process.env.DATABASE_TYPE.toLowerCase() : 'sqlite';
+const { db, initDatabase } = (databaseType === 'mysql')
   ? require('./database.mysql') 
   : require('./database.sqlite');
 
